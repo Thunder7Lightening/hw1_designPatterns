3b86a2dfa653fc867376eab57fda035656045579
Thunder7Lightening <31926495+Thunder7Lightening@users.noreply.github.com>
2017-10-03 16:05:14 +0800

    Add files via upload

--- number.h
+++ number.h
@@ -9,8 +9,9 @@ using std::string;
 
 class Number : public Constant
 {
-
+	string convertValueFromIntToString(int v){ return std::to_string(v); }
 public:
+	Number(int v):Constant(convertValueFromIntToString(v)){}
 	Number(string s):Constant(s){}
 };
 
