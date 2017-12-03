adb0c99c65014a8c5d0b5be54051f340fc39109c
Thunder7Lightening <31926495+Thunder7Lightening@users.noreply.github.com>
2017-12-03 17:43:47 +0800

    Add files via upload

--- iterator.h
+++ iterator.h
@@ -5,11 +5,12 @@
 #include "struct.h"
 #include "list.h"
 
+template<typename T>
 class Iterator {
 public:
   virtual void first() = 0;
   virtual void next() = 0;
-  virtual Term* currentItem() const = 0;
+  virtual T currentItem() const = 0;
   virtual bool isDone() const = 0;
 };
 
